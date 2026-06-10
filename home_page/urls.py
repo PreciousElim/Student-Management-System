32d80e80aef2e42d175c3e5fb18a14936a654e85
Precious Elim <preciouselimhingbovo@gmail.comhiigit config --global user.email preciouselimhingbovo@gmail.com>
2026-06-10 14:30:16 +0100

    home_page changes

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from accounts.views import CreateAccount
+from rest_framework.authtoken.views import obtain_auth_token
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', CreateAccount.as_view(), name='Creating Account'),
+    path('login/', obtain_auth_token, name='Login')
+]
