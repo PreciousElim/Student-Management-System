5d320e24ef2b5696d6afabb055b1b6adf9547e73
Precious Elim <preciouselimhingbovo@gmail.comhiigit config --global user.email preciouselimhingbovo@gmail.com>
2026-06-10 13:06:54 +0100

    Prepare project for deployment

--- sms/settings.py
+++ sms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t5srap(4*q5ub9h!azt%alblcqx_#(tkhb*u3(_i8z#^9hcetu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = 'accounts.User'
 
