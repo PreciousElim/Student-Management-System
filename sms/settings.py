4f589c73fd66cac1a8dbf7a937e90d3a9cccc3cf
Precious Elim <preciouselimhingbovo@gmail.comhiigit config --global user.email preciouselimhingbovo@gmail.com>
2026-06-15 14:42:12 +0100

    Changed Database

--- sms/settings.py
+++ sms/settings.py
@@ -99,7 +99,7 @@ from decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
