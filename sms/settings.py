b8a9df5dda429347fbc7f067e78e404993c532fb
Precious Elim <preciouselimhingbovo@gmail.comhiigit config --global user.email preciouselimhingbovo@gmail.com>
2026-06-16 13:49:01 +0100

    Improve security by removing hardcoded credentials

--- sms/settings.py
+++ sms/settings.py
@@ -20,15 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-dev-key-for-local-only"
-)
-
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+from decouple import config 
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = config('DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = ["*"]
 
